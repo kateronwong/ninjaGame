dda82461d91beb136fe5985513bbb9cc6d164cb0
Katerson wong <zxc676723@gmail.com>
2024-12-04 23:42:30 +0800

    CM v1

---
+++ ninja_game_v1/Assets/_Project/Script/Player/CharacterController/CharacterController.cs
@@ -0,0 +1,12 @@
+using CMF;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    //References to attached components;
+    protected Transform tr;
+    protected CharacterMovement CM;
+    //protected CharacterInput characterInput;
+
+
+}
